ab4082b84406d232b86876db48516115671ea54c
Suryarghya <suryar1332001@gmail.com>
2023-06-09 00:15:13 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Contact:&nbsp;{" "}
+        <a href="mailto:20je0998@cse.iitism.ac.in">20je0998@cse.iitism.ac.in</a>
+      </p>
+      <p>all rights reserved || 2023</p>
+    </footer>
+  );
+};
+
+export default Footer;
